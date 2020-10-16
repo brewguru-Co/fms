fb48721481f9a6623527a28308d0553db9380dc9
MincheolC <mccha0407@gmail.com>
2020-10-16 19:18:53 +0900

    feat: NotificationTarget API 연동

--- src/api/notificationTargets.js
+++ src/api/notificationTargets.js
@@ -1,22 +1,38 @@
-import axios from "axios";
-import config from "../config.json";
+import axios from 'axios';
+import config from '../config.json';
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 const { host, port } = config[env].api;
 
 export async function getNotificationTargets() {
-  const response = await axios.get(`${host}:${port}/notification_targets`);
+  // const response = await axios.get(`${host}:${port}/notification_targets`);
+  const response = await axios.get(`http://localhost:5000/notification-targets`);
   return response.data;
 }
 
 export async function deleteNotificationTarget(id) {
-  return id;
+  const response = await axios({
+    url: 'http://localhost:5000/notification-targets',
+    method: 'delete',
+    data: { id },
+  return response.data.id;
 }
 
 export async function postNotificationTarget(notificationTarget) {
-  return 5;
+  const response = await axios.post(
+    `http://localhost:5000/notification-targets`,
+    notificationTarget,
+  );
+  return response.data.id;
 }
 
 export async function patchNotificationTarget(notificationTarget) {
-  return notificationTarget;
+  const { id, name, email, phone, on } = notificationTarget;
+  const response = await axios.patch(`http://localhost:5000/notification-targets/${id}`, {
+    name,
+    email,
+    phone,
+    on,
+  });
+  return response.data;
 }
--- src/components/Dialog/NotificationTargetDialog.js
+++ src/components/Dialog/NotificationTargetDialog.js
@@ -1,18 +1,17 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import { NotificationTargetSchema } from "../../lib/formSchema";
-import styles from "../../assets/jss/components/notificationTargetDialogStyle";
-import locale from "../../locale/ko_KR.json";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Formik, Form } from 'formik';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { NotificationTargetSchema } from '../../lib/formSchema';
+import styles from '../../assets/jss/components/notificationTargetDialogStyle';
+import locale from '../../locale/ko_KR.json';
 
 const useStyles = makeStyles(styles);
 const NOTIFICATION = locale.NOTIFICATION;
@@ -23,95 +22,84 @@ export default function NotificationDialog(props) {
 
   const handleSave = (values, { setSubmitting }) => {
     setSubmitting(false);
-    onCreate(values);
     handleClose();
+    onCreate(values);
   };
 
   return (
-    <Dialog open={open} onClose={handleClose} maxWidth="xl">
-      <DialogTitle id="form-dialog-title">알림 추가</DialogTitle>
+    <Dialog open={open} onClose={handleClose} maxWidth='xl'>
+      <DialogTitle id='form-dialog-title'>알림 추가</DialogTitle>
       <DialogContent>
         <Formik
           initialValues={{
-            name: "",
-            email: "",
-            phone: "",
+            name: '',
+            email: '',
+            phone: '',
             on: true,
           }}
           validationSchema={NotificationTargetSchema}
           onSubmit={handleSave}
         >
-          {({
-            handleChange,
-            submitForm,
-            isSubmitting,
-            values,
-            touched,
-            errors,
-          }) => (
+          {({ handleChange, submitForm, isSubmitting, values, touched, errors }) => (
             <Form>
               <Box className={classes.box} margin={1}>
                 <TextField
                   autoFocus
                   required
-                  margin="dense"
-                  name="name"
+                  margin='dense'
+                  name='name'
                   label={NOTIFICATION.NAME}
-                  type="text"
+                  type='text'
                   onChange={handleChange}
                   error={touched.name && Boolean(errors.name)}
-                  helperText={touched.name ? errors.name : ""}
+                  helperText={touched.name ? errors.name : ''}
                 >
                   {errors.name}
                 </TextField>
                 <TextField
-                  margin="dense"
-                  name="email"
+                  margin='dense'
+                  name='email'
                   label={NOTIFICATION.EMAIL}
-                  type="email"
+                  type='email'
                   onChange={handleChange}
                   error={touched.email && Boolean(errors.email)}
-                  helperText={touched.email ? errors.email : ""}
+                  helperText={touched.email ? errors.email : ''}
                 >
                   {errors.email}
                 </TextField>
                 <TextField
-                  margin="dense"
-                  name="phone"
+                  margin='dense'
+                  name='phone'
                   label={NOTIFICATION.PHONE}
-                  type="tel"
+                  type='tel'
                   onChange={handleChange}
                   error={touched.phone && Boolean(errors.phone)}
-                  helperText={touched.phone ? errors.phone : "-없이 입력"}
+                  helperText={touched.phone ? errors.phone : '-없이 입력'}
                 />
                 <FormControlLabel
                   control={
                     <Checkbox
                       required
                       checked={values.on}
-                      id="on"
-                      type="checkbox"
+                      id='on'
+                      type='checkbox'
                       onChange={handleChange}
                     />
                   }
-                  label="알림"
-                  labelPlacement="top"
+                  label='알림'
+                  labelPlacement='top'
                 />
               </Box>
               <Box className={classes.box} margin={1}>
                 <Button
-                  variant="contained"
-                  color="primary"
+                  variant='contained'
+                  color='primary'
                   disabled={isSubmitting}
                   onClick={submitForm}
                 >
                   저장
                 </Button>
-                <Button
-                  variant="contained"
-                  onClick={handleClose}
-                  color="secondary"
-                >
+                <Button variant='contained' onClick={handleClose} color='secondary'>
                   취소
                 </Button>
               </Box>
